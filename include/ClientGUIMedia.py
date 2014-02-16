7407b43b6b91c1df5b61c69426aad93288be1de2
marcules <mharnos@gmail.com>
2014-02-16 18:53:27 +0100

    implemented selection inversion

--- include/ClientGUIMedia.py
+++ include/ClientGUIMedia.py
@@ -614,6 +614,15 @@ class MediaPanel( ClientGUIMixins.ListeningMediaList, wx.ScrolledWindow ):
         if select_type == 'all': self._DeselectSelect( [], self._sorted_media )
         else:
             
+            if select_type == 'invert':
+                _tmp_selected = set(self._selected_media)
+                _tmp_difference = HC.SortedList( self._sorted_media )
+
+                _tmp_difference.remove_items( _tmp_selected )
+                self._DeselectSelect( _tmp_selected, _tmp_difference )
+
+            else:
+            
             if select_type == 'none': ( media_to_deselect, media_to_select ) = ( self._selected_media, [] )
             else:
                 
@@ -1622,6 +1631,7 @@ class MediaPanelThumbnails( MediaPanel ):
                     select_menu.Append( CC.MENU_EVENT_ID_TO_ACTION_CACHE.GetId( 'select', 'archive' ), 'archive' )
                     
                 
+                select_menu.Append( CC.MENU_EVENT_ID_TO_ACTION_CACHE.GetId( 'select', 'invert' ), 'invert' )
                 select_menu.Append( CC.MENU_EVENT_ID_TO_ACTION_CACHE.GetId( 'select', 'none' ), 'none' )
                 
                 menu.AppendMenu( CC.ID_NULL, 'select', select_menu )
@@ -1941,6 +1951,7 @@ class MediaPanelThumbnails( MediaPanel ):
                         select_menu.Append( CC.MENU_EVENT_ID_TO_ACTION_CACHE.GetId( 'select', 'archive' ), 'archive' )
                         
                     
+                    select_menu.Append( CC.MENU_EVENT_ID_TO_ACTION_CACHE.GetId( 'select', 'invert' ), 'invert' )
                     select_menu.Append( CC.MENU_EVENT_ID_TO_ACTION_CACHE.GetId( 'select', 'none' ), 'none' )
                     
                     menu.AppendMenu( CC.ID_NULL, 'select', select_menu )
