4df69461f99a2fa7175565e764b55b246224ccc0
DonaldTsang <brandonknlee@gmail.com>
2017-05-17 21:55:02 +0800

    better comments

--- include/FanFicArchive.py
+++ include/FanFicArchive.py
@@ -1,31 +1,36 @@
 from bs4 import BeautifulSoup; from urllib import request; from re import match; import requests
-def ao3_rip(ao3id): # downloads whole story in one go
-    if isinstance(ao3id, int): ao3id = str(ao3id)
-    else: assert isinstance(ao3id, str)
-    r = request.urlopen('http://archiveofourown.org/works/'+ao3id+'?view_adult=true&view_full_work=true').read()
-    text = BeautifulSoup(r, "lxml")
+
+parser = "lxml"
+
+def fanfic_req(link): return BeautifulSoup(request.urlopen(link).read(), parser)
+def fanfic_check(id, name):
+    if isinstance(id, int): id = str(id)
+    else: assert isinstance(id, str), "Error for" + name + ": id not int or str"
+
+def ao3_rip(ao3_id): # downloads whole story in one go
+    fanfic_check(ao3_id, "ao3_rip")
+    text = fanfic_req('http://archiveofourown.org/works/'+ao3_id+'?view_adult=true&view_full_work=true')
     tag_bar = text.find("dl", class_="stats").find_all("dd") # everything in the top bar
     tag_list = {}
     for i in tag_bar:
-        x = [j.get_text() for j in i.find_all("a", class_="tag")]
-        if x == []: x = [i.get_text().lstrip('\n').lstrip(' ').rstrip(' ').rstrip('\n')]
-        tag_list[i.get("class")[0]] = x
+        tag = [j.get_text() for j in i.find_all("a", class_="tag")]
+        if tag == []: tag = [i.get_text().lstrip('\n').lstrip(' ').rstrip(' ').rstrip('\n')]
+        tag_list[i.get("class")[0]] = tag
     tag_list['author'] = [i.get_text() for i in text.find_all("a", rel="author")]
     summary = text.find_all("div", class_="summary module", role="complementary") # just below top bar
     chapter = {}
     for i in text.find_all("div", class_="chapter", id=True): # separating each chapters
         x = i.find("h3", class_="title").find("a").get("href").split("/")[-1]
         chapter[i.get('id')] = [x, i]
-    return ao3id, tag_list, summary, chapter
-def ff_rip(ffid): # no image
-    if isinstance(ffid, int): ffid = str(ffid)
-    else: assert isinstance(ffid, str)
-    r = request.urlopen("https://www.fanfiction.net/s/" + ffid +"/1").read() # first chapter
-    text = BeautifulSoup(r, "lxml")
+    return ao3_id, tag_list, summary, chapter
+
+def ff_rip(ff_id): # no image
+    fanfic_check(ff_id, "ff_rip")
+    text = fanfic_req("https://www.fanfiction.net/s/" +ff_id +"/1") # first chapter
     tag_line = text.find("span", class_="xgray xcontrast_txt").get_text().split(" - ") # top bar
     tag_list = {}
     for i in tag_line:
-        if ":" in i: a, b = i.split(": ")[0:2]; tag_list[a] = b.rstrip(' ')
+        if ":" in i: bracket, item = i.split(": ")[0:2]; tag_list[bracket] = item.rstrip(' ')
         else: tag_list[['Language', 'Genre', 'Characters'][tag_line.index(i)-1]] = i
     tag_list['Characters'] = tag_list['Characters'].split(', ')
     tag_list['Genre'] = tag_list['Genre'].split('/')
@@ -35,19 +40,16 @@ def ff_rip(ffid): # no image
     summary = text.find("div", class_="xcontrast_txt", style=True).get_text() # tag filtering
     chapter_list = {}
     for i in range(1, int(tag_list['Chapters'])+1): # loop through each chapter
-        r = request.urlopen("https://www.fanfiction.net/s/" + ffid + "/" + str(i)).read()
+        r = request.urlopen("https://www.fanfiction.net/s/"+ff_id+"/"+str(i)).read()
         storyid = match(".*storytextid=([0-9]+)", str(r)).group(1) # can't get it any other way
         text = BeautifulSoup(r, "lxml")
         story = text.find("div", class_="storytext xcontrast_txt nocopy", id="storytext") # literal lines
         chapter_list[i] = [storyid, story]
-    return ffid, userid, username, summary, tag_list, chapter_list
+    return ff_id, userid, username, summary, tag_list, chapter_list
+
 def da_rip(user_id, item_id): # name of literature can replace user_id, remember to bypass mature filters
-    if isinstance(user_id, int): user_id = str(user_id)
-    else: assert isinstance(user_id, str)
-    if isinstance(item_id, int): item_id = str(item_id)
-    else: assert isinstance(item_id, str)
-    r = request.urlopen("https://deviantart.com/art/" + user_id + "-" + itemid).read() # their url system is shit
-    text = BeautifulSoup(r, "lxml")
+    fanfic_check(user_id, da_rip); fanfic_check(item_id, da_rip)
+    text = fanfic_req("https://deviantart.com/art/"+user_id+"-"+itemid) # their url system is shit
     title = text.find("title").get_text() # title of the text
     pretext = text.find("li", class_="author") # description bar
     artist = pretext.find("a", class_="u regular username").get_text()
@@ -61,12 +63,11 @@ def da_rip(user_id, item_id): # name of literature can replace user_id, remember
     img = text.find("meta", property="og:image").get("content") # metadata at the top
     dick = text.find("div", class_="grf-indent").find("div", class_="text").contents[:-2] # js in text is not cool
     return title, artist, summary, idnum, type_tags, art_tags, img, dick
-def watt_rip(watt_id): # 100% queries, WTF?
-    if isinstance(watt_id, int): watt_id = str(watt_id)
-    else: assert isinstance(watt_id, str)
+
+def watt_rip(watt_id): # 100% queries, WTF? v4 is proprietary, WTF!
+    fanfic_check(watt_id, "watt_rip")
     book = requests.get('https://www.wattpad.com/apiv2/info?id=' + watt_id, headers={'User-Agent': 'Mozilla/5.0'}).json()
-    chapter_list = book['group']
-    result = {"tags": book["tags"].split(" ")}
+    chapter_list = book['group']; result = {"tags": book["tags"].split(" ")}
     for i in ["cover", "description", "groupId", "author", "completed"]: result[i] = book[i]
     for j in range(1, len(chapter_list)+1):
         chapter_id = str(chapter_list[j-1]['ID'])
--- include/YTArchive.py
+++ include/YTArchive.py
@@ -27,28 +27,33 @@ def set_pick(dash, container="mp4", resolution=[360,720], hfr=False):
     assert isinstance(hfr, bool), "error: hfr is not boolean"
     non_dash = [[18,22],[43]]; dash_audio = [[140], [171, 249, 250, 251]]
     dash_video = [
-        # 24,  36,  48,  72, 108, 72h,108h
-        [133, 134, 135, 136, 137, 288, 289],
-        [242, 243, 244, 247, 248, 302, 303]]
+        #240,  360,  480,  720, 1080, 720h,1080h
+        [133,  134,  135,  136,  137,  288,  289],
+        [242,  243,  244,  247,  248,  302,  303]]
     dash_video_flip = list(zip(*dash_video)) # reversing x and y axis
     table = {240:[0], 360:[1], 480:[2,5], 720:[3], 1080:[4,6]}
     unity = sum(dash_audio, []) + sum(dash_video, []) # all dash-media
     royale = unity + sum(non_dash, []) # all media
     def format_war(n): return dash_audio[n] + dash_video[n] + non_dash[n]
+    # set a: whether it is dash and/or non-dash
     if dash == True: a = unity
     elif dash == None:a = royale
     elif dash == False: a = sum(non_dash, []) # only non-dash-media
+    # set b: whether it is mp4 and/or webm
     if container == "mp4": b = format_war(0) # only mp4 or first bracket
     elif container == "webm": b = format_war(1) # only webm or second bracket
     elif container == "both": b = royale
+    # set c: whether it is 240, 360, 480, 720 and/or 1080
     c = [sum([dash_video_flip[j] for j in table[i]], ()) for i in resolution]
     c = [e for l in c for e in l] + sum(dash_audio, []) # pick and flatten
     if 360 in resolution: c += [18, 43] # adding necessary non-dash-media
     if 720 in resolution: c+= [22] # adding necessary non-dash-media
+    # set d: whether it is hi-frame-rate or not
     if hfr == True: d = royale
     elif hfr == False: d = sum(dash_audio, []) + # only standard-speed media
         [e for l in dash_video_flip[:5] for e in l] + sum(non_dash, [])
     return list(set(a) & set(b) & set(c) & set(d)) # intersects of sets
+
 def multi_downer(self, link): # please pipe from set_pick for best results
     for i in self: downer(str(i), link)
 
