1d2a3f214372ac8efae7cf93605481d64ea2e7e7
Hydrus Network Developer <hydrus.admin@gmail.com>
2023-05-09 16:59:55 -0500

    Docker v527

--- static/build_files/docker/server/entrypoint.sh
+++ static/build_files/docker/server/entrypoint.sh
@@ -6,11 +6,11 @@ GROUP_ID=${GID}
 echo "Starting Hydrus with UID/GID : $USER_ID/$GROUP_ID"
 
 stop() {
-  python3 /opt/hydrus/server.py stop -d="/opt/hydrus/db"
+  python3 /opt/hydrus/hydrus_server.py stop -d="/opt/hydrus/db"
 }
 
 trap "stop" SIGTERM
 
-su-exec ${USER_ID}:${GROUP_ID} python3 /opt/hydrus/server.py -d="/opt/hydrus/db" &
+su-exec ${USER_ID}:${GROUP_ID} python3 /opt/hydrus/hydrus_server.py -d="/opt/hydrus/db" &
 
 wait $!
