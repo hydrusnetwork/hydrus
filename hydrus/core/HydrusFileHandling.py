d92f3549faaba4068be52e39daad276596af0704
Minnowo <62451415+Minnowo@users.noreply.github.com>
2023-07-06 02:29:13 -0400

    Add support for krita files

--- hydrus/core/HydrusFileHandling.py
+++ hydrus/core/HydrusFileHandling.py
@@ -65,6 +65,8 @@ headers_and_mime.extend( [
     ( ( ( 0, b'8BPS\x00\x02' ), ), HC.APPLICATION_PSD ), # PSB, which is basically PSD v2 and does giganto resolution
     ( ( ( 0, b'CSFCHUNK' ), ), HC.APPLICATION_CLIP ),
     ( ( ( 0, b'SAI-CANVAS' ), ), HC.APPLICATION_SAI2 ),
+    ( ( ( 38, b'application/x-kritaPK' ), ), HC.APPLICATION_KRITA ), # important this comes before zip files because this is also a zip file
+    ( ( ( 58, b'application/x-kritaPK' ), ), HC.APPLICATION_KRITA ), # changes offset when compressed with zip64
     ( ( ( 0, b'PK\x03\x04' ), ), HC.APPLICATION_ZIP ),
     ( ( ( 0, b'PK\x05\x06' ), ), HC.APPLICATION_ZIP ),
     ( ( ( 0, b'PK\x07\x08' ), ), HC.APPLICATION_ZIP ),
