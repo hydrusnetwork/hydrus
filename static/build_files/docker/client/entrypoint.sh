6c9650fccf5a17379db43ff97acafc7501b329d4
Suika <2320837+Suika@users.noreply.github.com>
2025-10-15 00:41:20 +0200

    Docker: Bump Docker builds
    Replace x11vnc with tigervnc #1614, #1785. Try some permission settings. Extend the requests patch to 3.12.

--- static/build_files/docker/client/entrypoint.sh
+++ static/build_files/docker/client/entrypoint.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
-USER_ID=${UID}
-GROUP_ID=${GID}
+USER_ID=${UID:-1000}
+GROUP_ID=${GID:-1000}
 
-PYTHON_VERSION=$(python3 --version | awk '{print $2}')
-PYTHON_MAJOR_VERSION=$(echo $PYTHON_VERSION | cut -d. -f1)
-PYTHON_MINOR_VERSION=$(echo $PYTHON_VERSION | cut -d. -f2)
+echo "Executing entrypoint as: $(id)"
+groupmod --gid "$GROUP_ID" hydrus
+usermod --uid "$USER_ID" --gid "$GROUP_ID" hydrus
+echo "Hydrus will start with UID/GID : $USER_ID/$GROUP_ID"
 
-#apk add xterm
-echo "Starting Hydrus with UID/GID : $USER_ID/$GROUP_ID"
+if [ $USER_ID !=  1000 ] && [ $GROUP_ID != 1000 ]; then
+  echo "Modifying /opt/hydrus permissions, excluding /opt/hydrus/db/*"
+  find /opt/hydrus/ -path "/opt/hydrus/db/*" -prune -o -exec chown hydrus:hydrus "{}" \;
+  echo "Modifying /opt/noVNC permissions for consistency"
+  find /opt/noVNC/ -exec chown hydrus:hydrus "{}" \;
+  echo "Modifying /opt/venv permissions for consistency"
+  find /opt/venv/ -exec chown hydrus:hydrus "{}" \;
+fi
+
+PYTHON_MAJOR_VERSION=$(/opt/venv/bin/python3 -c "import sys; print(sys.version_info.major)")
+PYTHON_MINOR_VERSION=$(/opt/venv/bin/python3 -c "import sys; print(sys.version_info.minor)")
 
 cd /opt/hydrus/
 
@@ -22,15 +32,22 @@ if [ "$PYTHON_MAJOR_VERSION" == "3" ]; then
   if [ "$PYTHON_MINOR_VERSION" -lt 11 ]; then
     PATCH_FILE="/opt/hydrus/static/build_files/docker/client/requests.patch"
     if [ -f "$PATCH_FILE" ]; then
-      echo "Found and apply requests patch for py 3.10 and below"
-      cd $(python3 -c "import sys; import requests; print(requests.__path__[0])")
+      echo "Find and apply requests noproxy patch for py 3.10 and below"
+      cd $(/opt/venv/bin/python3 -c "import sys; import requests; print(requests.__path__[0])")
       patch -f -p2 -i "$PATCH_FILE"
     fi
   elif [ "$PYTHON_MINOR_VERSION" -eq 11 ]; then
     PATCH_FILE="/opt/hydrus/static/build_files/docker/client/requests.311.patch"
     if [ -f "$PATCH_FILE" ]; then
-      echo "Found and apply requests patch for py 3.11"
-      cd $(python3 -c "import sys; import requests; print(requests.__path__[0])")
+      echo "Find and apply requests noproxy patch for py 3.11"
+      cd $(/opt/venv/bin/python3 -c "import sys; import requests; print(requests.__path__[0])")
+      patch -f -i "$PATCH_FILE"
+    fi
+  elif [ "$PYTHON_MINOR_VERSION" -eq 12 ]; then
+    PATCH_FILE="/opt/hydrus/static/build_files/docker/client/requests.311.patch"
+    if [ -f "$PATCH_FILE" ]; then
+      echo "Find and apply requests noproxy patch for py 3.12"
+      cd $(/opt/venv/bin/python3 -c "import sys; import requests; print(requests.__path__[0])")
       patch -f -i "$PATCH_FILE"
     fi
   else
@@ -40,9 +57,5 @@ else
   echo "Unsupported Python major version: $PYTHON_MAJOR_VERSION"
 fi
 cd /opt/hydrus/
-
-#if [ $USER_ID !=  0 ] && [ $GROUP_ID != 0 ]; then
-#  find /opt/hydrus/ -not -path "/opt/hydrus/db/*" -exec chown hydrus:hydrus "{}" \;
-#fi
 
 exec supervisord -c /etc/supervisord.conf
